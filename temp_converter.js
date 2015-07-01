6d6bb2c5fbf26e8f3fd92590582372b3974c621e
Alex Bannon <alexbannon@gmail.com>
2015-07-01 16:47:42 -0400

    changed temp converter

--- temp_converter.js
+++ temp_converter.js
@@ -1,12 +1,32 @@
 // Set this variable to a starting Fahrenheit temperature...
-var fahrenheit = 32;
-
-// Enter conversion code below...
-var fahrenheitToCelsius = ...;
-var fahrenheitToKelvin = ...;
-
-// Enter console.log statements below...
-
-// Repeat the above for Celsius and Kelvin...
+var whichTemp = "f";
+var fahrenheit = 100;
 var celsius = 0;
 var kelvin = 273.15;
+var fahrenheitTemps = [];
+var celsiusTemps = [];
+var kelvinTemps = [];
+if(whichTemp = "f"){
+// Enter conversion code below...
+fahrenheitTemps.push(fahrenheit);
+fahrenheitTemps.push((fahrenheit - 32) / 1.8);
+fahrenheitTemps.push((fahrenheit + 459.67) / 1.8);
+for(var x = 0; x < fahrenheitTemps.length; x++) {
+  console.log(fahrenheitTemps[x]);
+}
+// Repeat the above for Celsius and Kelvin...
+}
+else if(whichTemp = "c") {
+
+celsiusTemps.push(celsius);
+celsiusTemps.push((celsius * 1.8 + 32))
+celsiusTemps.push(celsius + 273.15);
+
+console.log(celsiusTemps);
+}
+else {
+kelvinTemps.push(kelvin);
+kelvinTemps.push(kelvin * 1.8 - 459.67);
+kelvinTemps.push(kelvin - 273.15);
+console.log(kelvinTemps);
+}
